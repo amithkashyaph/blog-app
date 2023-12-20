1d66548b42c24ffa9f8132a4d58957dc8f63fa41
amithkashyap <amithkashyaprv@gmail.com>
2023-12-20 11:55:19 +0530

    added Post repository interface

--- src/main/java/com/project/blogapp/repository/PostRepository.java
+++ src/main/java/com/project/blogapp/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.project.blogapp.repository;
 import com.project.blogapp.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByCategoryId(Long id);
 }
