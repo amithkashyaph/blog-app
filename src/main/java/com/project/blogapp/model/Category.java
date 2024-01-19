92320381507316b92752a853a5f3d516cc13c0d8
amithkashyap <amithkashyaprv@gmail.com>
2024-01-19 11:28:53 +0530

    added Category model file

---
+++ src/main/java/com/project/blogapp/model/Category.java
@@ -0,0 +1,26 @@
+package com.project.blogapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Post> posts;
+
+}
